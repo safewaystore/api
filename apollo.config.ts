17dfc0494629126c011314f56ecc8940678c6cba
Carlos Oliveira <carlosoliveiradev@gmail.com>
2019-08-26 09:08:47 -0300

    Apollo config changes

--- apollo.config.ts
+++ apollo.config.ts
@@ -5,11 +5,3 @@ module.exports = {
     url: 'http://localhost:4000/graphql',
   },
 };
-
-// module.exports = {
-//   client: {
-//     service: {
-//       url: 'https://{myendpoint}/graphql'
-//     }
-//   }
-// };
