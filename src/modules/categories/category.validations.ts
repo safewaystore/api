8753a7d193d48b8ab08cc16456bd19e1119ce75e
Carlos Oliveira <carlosoliveiradev@gmail.com>
2019-08-30 20:42:04 -0300

    Fix create category space validation

--- src/modules/categories/category.validations.ts
+++ src/modules/categories/category.validations.ts
@@ -2,10 +2,16 @@ import * as yup from 'yup';
 
 export const createCategorySchema = () =>
   yup.object().shape({
-    title: yup.string().required(),
+    title: yup
+      .string()
+      .trim()
+      .required(),
   });
 
-export const updateCategorySchema = () => 
+export const updateCategorySchema = () =>
   yup.object().shape({
-    title: yup.string().required(),
+    title: yup
+      .string()
+      .trim()
+      .required(),
   });
